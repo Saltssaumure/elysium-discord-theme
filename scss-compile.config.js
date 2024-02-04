375491707c3882067c6ca858a22ea34ed3c95325
Saltssaumure <Saltssaumure@users.noreply.github.com>
2024-02-04 06:55:18 +0000

    deploy: 2a93b294b279457bb08167137ecdeae64c897cf3

---
+++ scss-compile.config.js
@@ -0,0 +1,9 @@
+/** @type {import('bd-scss/lib/config').Config} */
+export default {
+    meta: {
+        name: "Discord Elysium" /* The official name of your theme. */,
+        scss: "elysium" /* The name of your base scss file. */,
+        repo: "elysium-discord-theme" /* The name of your theme repository. */,
+        version: "dev" /* The version of your theme. */
+    }
+};
